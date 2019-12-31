b0fe9c32e5ffd61967bf07e53e396709698f6507
linwu.liao <linwu.liao@xuncetech.com>
2019-12-31 16:05:29 +0800

    init

---
+++ src/main/java/com/xunce/model/Student.java
@@ -0,0 +1,49 @@
+package com.xunce.model;
+
+public class Student {
+    private String name;
+    private Integer age;
+    private String grade;
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
+
+    public Student(String name, Integer age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
